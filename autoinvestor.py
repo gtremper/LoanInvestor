24c8ced71ecfcf2b93b6465d9c271e6957293587
Graham Tremper <gtremper@gmail.com>
2014-07-08 22:52:42 -0700

    Added loan volume plotter

--- autoinvestor.py
+++ autoinvestor.py
@@ -8,8 +8,9 @@ from collections import Counter
 from dateutil import parser
 
 import csv
-import datetime
+import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import time
 import urllib
 
@@ -34,14 +35,17 @@ def load_historical_data():
   with open(HISTORICAL_DATA_PATH, "r") as f:
     return list(csv.DictReader(f))
 
+def read_loans():
+  return urllib.urlopen("https://resources.lendingclub.com/secure/primaryMarketNotes/browseNotes_1-RETAIL.csv")
+
 def main():
   # Read csv enpoint from lendingclub
-  resource = urllib.urlopen("https://resources.lendingclub.com/secure/primaryMarketNotes/browseNotes_1-RETAIL.csv")
+  resource = read_loans()
   notes = csv.DictReader(resource)
 
   # get list of datetimes for current notes
   list_dates = [parser.parse(x['list_d']) for x in notes]
-  list_dates = map(lambda x: x-datetime.timedelta(minutes=x.minute, seconds=x.second) ,list_dates)
+  list_dates = map(lambda x: x-dt.timedelta(minutes=x.minute, seconds=x.second) ,list_dates)
 
   #count unique times
   c = Counter(list_dates)
@@ -56,7 +60,7 @@ def main():
   
 
 def funded():
-  resource = urllib.urlopen("https://resources.lendingclub.com/secure/primaryMarketNotes/browseNotes_1-RETAIL.csv")
+  resource = read_loans()
   notes = csv.DictReader(resource)
   notes = [x for x in notes]
 
@@ -79,26 +83,25 @@ def plot_total_funded():
   plt.show()
 
 def round_time_to_last_release(t):
-  rounded = t - datetime.timedelta(minutes=t.minute, seconds=t.second, microseconds=t.microsecond)
+  rounded = t - dt.timedelta(minutes=t.minute, seconds=t.second, microseconds=t.microsecond)
   if t.hour > 18 or t.hour < 6:
-    rounded -= datetime.timedelta(hours=t.hour-18)
+    rounded -= dt.timedelta(hours=t.hour-18)
   elif t.hour > 14:
-    rounded -= datetime.timedelta(hours=t.hour-14)
+    rounded -= dt.timedelta(hours=t.hour-14)
   elif t.hour > 10:
-    rounded -= datetime.timedelta(hours=t.hour-10)
+    rounded -= dt.timedelta(hours=t.hour-10)
   else:
-    rounded -= datetime.timedelta(hours=t.hour-6)
+    rounded -= dt.timedelta(hours=t.hour-6)
 
   return rounded
 
 def check_new_loans():
-
-    resource = urllib.urlopen("https://resources.lendingclub.com/secure/primaryMarketNotes/browseNotes_1-RETAIL.csv")
+    resource = read_loans()
     loans = list(csv.DictReader(resource))
-    now = round_time_to_last_release(datetime.datetime.now())
+    now = round_time_to_last_release(dt.datetime.now())
     if any(parser.parse(x['list_d']) > now for x in loans):
       print "New Loans!!! "
-      print datetime.datetime.now()
+      print dt.datetime.now()
       print "########################"
       new_loans = filter(lambda x: parser.parse(x['list_d']) > now, loans)
 
@@ -108,13 +111,31 @@ def check_new_loans():
 
       return True
     else:
-      print "no new loans " + str(datetime.datetime.now())
+      print "no new loans " + str(dt.datetime.now())
       return False
 
+def poll_for_loans():
+  while not check_new_loans():
+    pass
+
+def num_available_loans():
+  return sum(1 for x in read_loans())
+
+def plot_available_loans(delta=dt.timedelta(minutes=2)):
+  poll_for_loans()
+
+  num_loans = []
+
+  start_time = dt.datetime.now()
+  while dt.datetime.now() < start_time + delta:
+    num_loans.append(num_available_loans())
+    time_remaining = start_time + delta - dt.datetime.now()
+    print len(num_loans), str(time_remaining.seconds) + " seconds remaining"
+
+  plt.plot(num_loans)
+  plt.show()
 
 
 if __name__ == '__main__':
-  while not check_new_loans():
-    pass
-  print "done!"
+  plot_available_loans(dt.timedelta(minutes=2))
 
