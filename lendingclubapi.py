3e60e3fb63a8c09b78a5197dd774a8d86252f4f2
Graham Tremper <gtremper@gmail.com>
2014-10-17 23:21:32 -0700

    Added available_cash to lendingclubapi

--- lendingclubapi.py
+++ lendingclubapi.py
@@ -2,19 +2,55 @@
 import urllib2
 import json
 
-
 class API:
   """Provides and interface to the LendingClub REST API"""
 
-  _BASE_URL = 'https://api.lendingclub.com/api/investor/v1/accounts/'
+  _API_VERSION = "v1"
+  _LOAN_URL = "https://api.lendingclub.com/api/investor/{}/loans/listing"\
+              .format(_API_VERSION)
 
-  def __init__(self, investor_id, key):
-    self.investor_id = id  # Account id
-    self.key = key # Api key
+  def __init__(self, investor_id, api_key):
+    self.investor_id = investor_id
+    self.api_key = api_key
+    self._base_url ='https://api.lendingclub.com/api/investor/{}/accounts/{}/{}'\
+                    .format(API._API_VERSION, investor_id, '{}')
 
-  
+  def available_cash(self):
+    """Get the availble cash in your account"""
+    req = urllib2.Request(self._base_url.format("availablecash"))
+    req.add_header('Authorization', self.api_key)
+    try:
+      data = json.load(urllib2.urlopen(req))
+    except urllib2.HTTPError as e:
+      print e
+      return None
+
+    return data['availableCash']
 
 
+
+
+
+
+
+def load_api(investor_id_path, api_key_path):
+  """ create an api instance from secrets stored in files
+
+  investor_id_path -- path to file containing investor id
+  api_key_path -- path to file containing api secret key
+  """
+
+  with open(investor_id_path) as f:
+    investor_id = f.read()
+
+  with open(api_key_path) as f:
+    api_key = f.read()
+
+  return API(investor_id, api_key)
 
 if __name__ == '__main__':
+  api = load_api("investor_id.txt", "api_key.txt")
+  print 'cash:', api.available_cash() 
+
+
   
