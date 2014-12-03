110c29551e0b303dd889280e39554302c025b4f9
Graham Tremper <gtremper@gmail.com>
2014-12-03 02:20:59 -0800

    Added p2p loan polling and listed loans comparison

--- p2ppicks.py
+++ p2ppicks.py
@@ -4,13 +4,20 @@ Invest in loans via using p2ppicks as an underwriter
 """
 
 import lendingclub as lc
+import datetime as dt
+import dateutil.parser as dateparser
 import hashlib
 import json
+import time
 import urllib
 import urllib2
+import pprint
 
 class P2PPicks(lc.Api):
   _BASE_URL = "https://www.p2p-picks.com/api/v1/{method}/{action}"
+
+  # Rate limit for the api
+  RATE_LIMIT = dt.timedelta(seconds=1.0)
 
   def __init__(self, secrets='data/secrets.json'):
     with open(secrets) as f:
@@ -39,17 +46,79 @@ class P2PPicks(lc.Api):
     req = urllib2.Request(P2PPicks._BASE_URL.format(method=method, action=action))
     req.add_data(urllib.urlencode(data))
 
-    return json.load(urllib2.urlopen(req))
+    return json.load(urllib2.urlopen(req))['response']
 
   def list(self):
-    res = self.request('picks', 'list', {'p2p_product': 'profit-maximizer'})
-    return res
+    data = self.request('picks', 'list', {'p2p_product': 'profit-maximizer'}) 
+    return data['picks'], dateparser.parse(data['timestamp'])
+
+  def poll_picks(self):
+    """Rate limited generator of currently listed loans"""
+    while True:
+      call_time = dt.datetime.now()
+      try:
+        yield self.list()
+      except urllib2.HTTPError as err:
+        print "HTTPError: {}".format(err.code)
+        time.sleep(2)
+      except urllib2.URLError as err:
+        print "URLError: {}".format(err.reason)
+        time.sleep(2)
+      except (KeyboardInterrupt,SystemExit) as err:
+        print "err", err
+        return
+      except Exception as err:
+        print "Other exception:", type(err), err
+        time.sleep(5)
+
+      # Sleep until ready again
+      sleep_time = call_time - dt.datetime.now() + self.RATE_LIMIT
+      if sleep_time > dt.timedelta(0):
+        time.sleep(sleep_time.total_seconds())
+
+  def poll_for_update(self):
+    start = dt.datetime.now()
+    print "Starting poll at", start
+    print
+
+    for picks, timestamp in self.poll_picks():
+      if timestamp < start:
+        continue
+      return picks
+
+  def check_loans_available(self):
+    picks = self.poll_for_update()
+    top = filter(lambda x: x['top'] == '5%', picks)
+
+    p2p_ids = set(int(i['loan_id']) for i in top)
+    print "top 5% ids"
+    print "P2P IDs:", p2p_ids
+
+    listed = set(x['id'] for x in self.listed_loans())
+
+    availble = p2p_ids & listed
+    print dt.datetime.now().time()
+    print "Available:", availble
+
+    print 
+    print "Polling for availability"
+    while True:
+      time.sleep(1)
+
+      listed = set(x['id'] for x in self.listed_loans())
+      print dt.datetime.now().time(), p2p_ids & listed
+    
+
+
+
+
+
 
 
 
 def main():
   p2p = P2PPicks()
-  print p2p.list()
+  p2p.check_loans_available()
 
 if __name__ == '__main__':
   main()
